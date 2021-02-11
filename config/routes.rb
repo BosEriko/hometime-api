934085c8180772ddfdead7d3d2307b6d216ecf1b
BosEriko <bos.eriko@gmail.com>
2021-02-11 16:51:17 +0800

    Feature: Add routes for guests under a specific reservation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :guests
-  resources :reservations
+  resources :reservations do
+    resources :guests
+  end
   root to: redirect("https://hometime.io/")
 end
